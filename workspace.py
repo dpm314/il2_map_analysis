081abf6237aa3b163e5e3ea75b8ce4cd7e3351c7
DrDave <meichled@gmail.com>
2019-12-10 01:43:02 -0500

    hacky but working way to detect inds of forest, city, water, clear

--- workspace.py
+++ workspace.py
@@ -2,8 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
-
 Image.MAX_IMAGE_PIXELS = None
+#%%
+def pixelValueHistogram(I):
+    #returns pixelCount[i,j]
+    #where   pixelCount[i,j] is number of times the value i occurs in layer j
+    pixelCount = np.zeros((255,3))
+    for i in range(3):
+        for j in range(255):
+            print(i,j)
+            pixelCount[j,i] = np.sum(np.where(I[:,:,i] == j))
+    return pixelCount
+#%%
+
 #%%
 #Map raw .png files can be downloaded from: http://tiles.il2missionplanner.com/stalingrad/stalingrad.png #etc for the four map names
 mapDirectoryRoot = '/home/dpm314/coconut/maps/'
@@ -55,16 +66,48 @@ k = np.where( (nparr == pixelInds[0]) | (nparr == 50) | (nparr == 124) | (nparr 
 # np_array_version = np.asarray(im)
 #%%
 import imageio
+import copy
+#%%
 II = imageio.imread(mapFileNames[-1])
 #%%
-II = II[1200:2000,2500:5500,:]
+plt.close('all')
+I = np.array(II[1000:3000,2800:5500,:], dtype = np.int16)
 colorMap = {'clear' :np.array( [211,209,197], dtype = np.int16), 
             'forest':np.array( [189,186,162], dtype = np.int16),
-            'water' :np.array( [211,209,197], dtype = np.int16)
+            'water' :np.array( [161,186,197], dtype = np.int16),
+            'city'  :np.array( [165,165,165], dtype = np.int16)
             }
+
+
+#delta = np.abs((I - colorMap['water'])).sum(axis = 2)
+key = 'city'
+delta = np.zeros([I.shape[0],I.shape[1]])
+for i in range(I.shape[0]):
+    for j in range(I.shape[1]):
+        delta[i,j] = np.sum(np.abs(I[i,j,:] - colorMap[key]))
+plt.figure();plt.imshow(delta)
+
+
+
+
+f = copy.copy(I)
+threshold= 15
+
+mask = np.where(delta <threshold, 0,1)
+
+plt.figure(); plt.imshow(mask)
+
+f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],0] = 0
+f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],1] = 0
+f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],2] = 0
+plt.imshow(f)
 #%%
-I = II
-k = np.where( II )
+
+
+
+
+
+
 
 
 
