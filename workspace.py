296d83e60f12f0cf9b6b5d791cedbba474a7f513
DrDave <meichled@gmail.com>
2019-12-27 11:57:10 -0500

    working! added (optional) raw image and mask filter dicts, might need to adject for other maps

--- workspace.py
+++ workspace.py
@@ -1,14 +1,13 @@
 #Map image .png files can be downloaded from: http://tiles.il2missionplanner.com/stalingrad/stalingrad.png #etc for the four map names
-#mapDirectoryRoot = '/home/dpm314/coconut/maps/'
-
+#mapDirectoryRoot = '/home/dpm314/coconut/maps/' #mapDirectoryRoot = '/home/dpm314/il2_map_analysis/maps/'
 import numpy as np
 import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
 from PIL import ImageFilter
 Image.MAX_IMAGE_PIXELS = None
-#%%###########################################################################
-mapDirectoryRoot = '/home/dpm314/il2_map_analysis/maps/'
+#%%################# Configuration ###################################
+directoryRoot = '/home/dpm314/coconut/'
 mapNames = ['moscow','stalingrad','kuban','rheinland']
 
 colorMap = {'forest':np.array( [189,186,162], dtype = np.int16),
@@ -16,38 +15,36 @@ colorMap = {'forest':np.array( [189,186,162], dtype = np.int16),
             'city'  :np.array( [165,165,165], dtype = np.int16)
             }
 thresholds = {'forest'  :np.int16(25),
-              'water'   :np.int16(30),
-              'city'    :np.int16(10)
-            }
-medianFilters = {'forest' :ImageFilter.MedianFilter(size = 11),
-                'water'   :ImageFilter.MedianFilter(size = 3 ),
-                'city'    :ImageFilter.MedianFilter(size = 5 )
+              'water'   :np.int16(20),
+              'city'    :np.int16(1)
             }
 
-medianFilters = {'forest'  :None,
-                'water'   : None,
-                'city'    : None
+mapFilters = {'forest'  :ImageFilter.ModeFilter( size = 5 ),#ImageFilter.ModeFilter( size = 3 ),
+              'water'   :None,
+              'city'    :ImageFilter.ModeFilter( size = 3 )
             }
-
+maskFilters = {'forest' :None,
+              'water'   :ImageFilter.MinFilter( size = 5 ),
+              'city'    :None
+            }
 mapDimensions = {
     'moscow'    : None, #todo: measure the other maps
     'stalingrad': None,
     'kuban'     : None,
     'rheinland' : (40*10*1000.0,40*10*1000.0) #40 squares, 10 km/square, 1000 meters/km
     }
-
-def fixMapCoordinates(pixelIndices, img, downsampleRate = 1):
+#%%################# Utility Functions ###################################
+def fixMapCoordinates(pixelIndices, imgSize):
     coordinates = np.zeros( pixelIndices.shape )
-    coordinates[:,1] = img.size[1]  - pixelIndices[:,0]
+    coordinates[:,1] = imgSize[1]  - pixelIndices[:,0]
     coordinates[:,0] = pixelIndices[:,1]
-    coordinates = coordinates[::downsampleRate,:]
     return coordinates
 
-def writeCoordinatesToFile(coords,filename_base = 'coordinates_', path_base = '/home/dpm314/il2_map_analysis/data/'):
+def writeCoordinatesToFile(coords,filename_base = 'coordinates_', path_base = directoryRoot):
     for key in coords.keys():
-        fname = path_base + '{}{}.csv'.format(filename_base, key)
+        fname = path_base + 'data/' + '{}{}.csv'.format(filename_base, key)
         print("Writing Coordinates to file: {}".format(fname))
-        np.savetxt(fname,coordinates[key],delimiter=',',fmt='%7i',header='test header')
+        np.savetxt(fname,coordinates[key],delimiter=',',fmt='%7i')
 
 def pixelsToMeters(pixel, mapDimension ):
     #normalize first then expand to meters
@@ -59,149 +56,55 @@ def pixelsToMeters(pixel, mapDimension ):
     y = np.int32(y) 
     return x,y
 
-#%%############################################################################
-mapFileNames = [mapDirectoryRoot + mapName + '.png' for mapName in mapNames]
-mapIndex = -1
-img = Image.open(mapFileNames[mapIndex])
-
-img = img.resize( (img.size[0],
-                   img.size[1],) )
-plt.imshow(img)
-
-
+#%%################ Processing Code #########################################
+mapFileNames = [directoryRoot + 'maps/' + mapName + '.png' for mapName in mapNames]
+mapIndex = -1 #start with just rheinland
+img = Image.open(mapFileNames[mapIndex]).crop([2500,2500,4001,4001])
 #%%
 plt.close('all')
 masks = {}
-img_as_array = np.asarray(img) 
-for key in colorMap.keys():
-    print('Generating masks for {} : {}'.format(mapNames[mapIndex], key))
-    mask = np.int16(np.sum( np.abs( img_as_array - colorMap[key]), axis = 2))
-    mask = np.where(mask < thresholds[key], np.int16(0), mask )
-    mask = Image.fromarray(mask)
-    if medianFilters[key] is not None:
-        mask = mask.filter(medianFilters[key])
-    masks[key] = mask
-    #plt.figure()
-    #plt.imshow(masks[key])
-#%%
-locations = {}
+diff = {}
 coordinates = {}
 for key in colorMap.keys():
-    print('Locating for {} : {}'.format(mapNames[mapIndex], key))
-    #mask_as_array = np.array( masks[key].getdata(), dtype = np.int16 ).reshape( [masks[key].size[0], masks[key].size[1]] )
-    mask_as_array  = np.asarray(masks[key], dtype = np.int16)
-    locations[key] = np.argwhere( mask_as_array == 0 )
-    locations[key] = fixMapCoordinates( locations[key], masks[key], 1 )
-    coordinates[key] = pixelsToMeters( locations[key], mapDimensions[mapNames[mapIndex] ] )
-    
-#%%
-for key in coordinates.keys():
-    writeCoordinatesToFile(coordinates[key])
+    print('Generating masks for {} : {}'.format(mapNames[mapIndex], key))
+    #Pre-process filter map image if needed
+    if mapFilters[key] is not None:
+        img_array = np.asarray(img.filter(mapFilters[key]))
+    else:
+        img_arrag = np.asarray(img)
+    print('.')
+    #Compute each pixels similarity to key (water, city or forest templates in colorMap)
+    diff[key] = np.int16(np.sum( np.abs( img_array - colorMap[key]), axis = 2))
+    #Create a mask from the difference, zero pixels below a threshold
+    mask = np.where(diff[key] < thresholds[key], np.int16(0), img.convert('I') )
+    print('..')
+    #Post-process filter mask if needed
+    if maskFilters[key] is not None:
+        #Convert back to Image and filter
+        mask = Image.fromarray(mask).filter(maskFilters[key])
+        #convert back to numpy array 
+        mask = np.asarray(mask, dtype = np.int16)
+    #optional plot the mask:
+    #plt.figure() #plt.imshow(masks[key])
+    #Find indices of all zeros in the mask (where key is)
+    print('    Locating {}'.format(key))
+    locations = np.argwhere( mask == 0 )
+    locations = fixMapCoordinates( locations, img.size)
+    #Convert to meters from the South-West corner (increasing X goes East and increasing Y goes North)
+    coordinates[key] = pixelsToMeters( locations, mapDimensions[mapNames[mapIndex] ] )
+    #store mask for debug & display
+    masks[key] = mask
+#Write to .csv file
 
-#%%
-#useful stuff in Image.
-# .composite() blends two images with a transparency mask
-# .merge() combines bands (R,G,B) in to a multiband image
-# .fromarray() make image from numpy array 
-# .filter(filter)
-# np_array_version = np.asarray(im)
-#%%
-import imageio
-import copy
-#%%
-II = imageio.imread(mapFileNames[-1])
+writeCoordinatesToFile(coordinates)
+
 #%%
 plt.close('all')
-I = np.array(II[1000:3000,2800:5500,:], dtype = np.int16)
-colorMap = {'clear' :np.array( [211,209,197], dtype = np.int16), 
-            'forest':np.array( [189,186,162], dtype = np.int16),
-            'water' :np.array( [161,186,197], dtype = np.int16),
-            'city'  :np.array( [165,165,165], dtype = np.int16)
-            }
-
-
-#delta = np.abs((I - colorMap['water'])).sum(axis = 2)
-key = 'forest'
-#delta = np.zeros([I.shape[0],I.shape[1]])
-#for i in range(I.shape[0]):
-#    for j in range(I.shape[1]):
-#        delta[i,j] = np.sum(np.abs(I[i,j,:] - colorMap[key]))
-delta = np.sum(np.abs( I - colorMap[key]), axis = 2, dtype = np.int16)
-plt.figure();plt.imshow(delta)
-
-
-thresholds = {'clear' :np.int16(10), 
-            'forest'  :np.int16(30), 
-            'water'   :np.int16(10), 
-            'city'    :np.int16(10)
-            }
-
-f = copy.copy(I)
-
-
-mask = np.int16( np.where(delta <thresholds[key], 0,1))
-#%%
-
-#%%
-#for k in range(3):
-#    f[:,:,k] *= mask
-#%%
-medianFilter = PIL.ImageFilter.MedianFilter(size = 9)
-
-filteredMask = PIL.Image.fromarray(mask).filter(medianFilter)
-plt.figure()
-plt.imshow(filteredMask)
-for k in range(3):
-    f[:,:,k] *= filteredMask
-plt.figure()
-plt.imshow(f)
-
-#%%
-
-
-plt.figure(); plt.imshow(mask)
-
-f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],0] = 0
-f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],1] = 0
-f[np.where(delta<threshold)[0],np.where(delta<threshold)[1],2] = 0
-plt.imshow(f)
-#%%
-deltaImage = PIL.Image.fromarray(delta)
-
-#%% Apply median filter to remove small artifacts
-from PIL import ImageFilter
-medianFilter = PIL.ImageFilter.MedianFilter(size = 25)
-deltaFiltered = deltaImage.filter(medianFilter)
-plt.figure()
-plt.imshow(deltaFiltered)
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-#%%
-img = Image.open(mapFileNames[-1])
-img = img.crop((8000,8000,10000,10000))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for key in masks.keys():
+    plt.figure(); plt.imshow(masks[key]); plt.title(key)
+    plt.savefig(directoryRoot + key+'_example.png')
+    plt.figure(); plt.plot(coordinates[key][0], coordinates[key][1],'.')
+    plt.xlabel('West->East (m)', fontsize = 16)
+    plt.ylabel('South->North (m)', fontsize = 16)
+    plt.tight_layout()
+    plt.savefig(directoryRoot + key+'_coordinates.png')
